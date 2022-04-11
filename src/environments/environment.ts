5461f9241b5d340379e5af1a6bafbc959f2b0183
mickacharp <mickaelcharp@gmail.com>
2022-04-11 12:23:13 +0200

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'quiz-confiance',
+    appId: '1:320325356822:web:e4a7ef0da0fd4ff3511437',
+    storageBucket: 'quiz-confiance.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyA0_frtylwqfBtVtvIUeplwIETjSyk4W34',
+    authDomain: 'quiz-confiance.firebaseapp.com',
+    messagingSenderId: '320325356822',
+    measurementId: 'G-SJKE7F7Q47',
+  },
   production: false
 };
 
